4704ee3314784c8f6ce5988db96a1a645e394ed5
andear <andear@andeardeMacBook-Pro.local>
2018-01-27 00:55:50 -0600

    adding javacript

--- ipod.js
+++ ipod.js
@@ -1,29 +1,88 @@
 // Create your global variables below:
 var tracklist = ["Let It Happen", "Nangs", "The Moment", "Yes I'm Changing", "Eventually", "Gossip", "The Less I Know The Better", "Past Life", "Disciples", "'Cause I'm A Man"];
 var volLevels = [];
+var volume = 3;
+//0 for status: stop
+//1 for status: play
+var status = 0;
+var currentTime = 0;
+var iter = 6;
 
 function init() {
-	// Your code goes here
-};
+    //initial the <input> element
+    var temp = document.getElementById("player-time");
+    temp.value = 0;
+    temp.step = "1";
+    temp.min = 0;
+    temp.max = 180;
+    //initial the first three volume level to have color
+    for(i = 0; i < volume; i++){
+        document.getElementById("vl" + i).style.backgroundColor = "#9f5cc4";
+    }
+
+}
 
 function volUp() {
-	// Your code goes here
+    document.getElementById("vl" + volume).style.backgroundColor = "#9f5cc4";
+    //handle the corner case
+    if(volume == 5){
+        return;
+    }
+    volume++;
 }
 
 function volDown() {
-	// Your code goes here
+    document.getElementById("vl" + volume).style.backgroundColor = "white";
+    if(volume==0){
+        return;
+    }
+    volume --;
 }
 
 function switchPlay() {
-	// Your code goes here
+    //switch the icon
+    var switchIcon = document.getElementById("switch");
+    if(status == 0){
+        switchIcon.innerHTML = "<i class=\"material-icons\">pause</i> ";
+        status = 1;
+    }
+    else{
+        switchIcon.innerHTML = "<i class=\"material-icons\">play_arrow</i> ";
+        status = 0;
+    }
 }
 
 function nextSong() {
-	// Your code goes here
+	currentTime = 0;
+	status = 1;
+    document.getElementById("switch").innerHTML = "<i class=\"material-icons\">pause</i> ";
+	var Name = document.getElementById("player-song-name");
+	var newName;
+	if(iter == 9){
+	    iter = 0;
+    }
+    else{
+	    iter ++;
+    }
+    var newName = tracklist[iter];
+	Name.innerText = newName;
+
 }
 
 function prevSong() {
-	// Your code goes here
+    currentTime = 0;
+    status = 1;
+    document.getElementById("switch").innerHTML = "<i class=\"material-icons\">pause</i> ";
+    var Name = document.getElementById("player-song-name");
+    var newName;
+    if(iter == 0){
+        iter = 9;
+    }
+    else{
+        iter --;
+    }
+    var newName = tracklist[iter];
+    Name.innerText = newName;
 }
 
 function secondsToMs(d) {
@@ -35,4 +94,21 @@ function secondsToMs(d) {
     return `0${min}`.slice(-1) + ":" + `00${sec}`.slice(-2);
 }
 
+function updateTime() {
+    if(status == 0){
+        return;
+    }
+    var temp = document.getElementById("player-time");
+    temp.value = currentTime;
+    var timeElapsed = document.getElementById("time-elapsed");
+    timeElapsed.innerText = secondsToMs(currentTime);
+    if(currentTime == 180){
+        nextSong();
+    }
+    currentTime ++;
+
+}
+
+
 init();
+var seconds = setInterval(function(){ updateTime() }, 1000);
